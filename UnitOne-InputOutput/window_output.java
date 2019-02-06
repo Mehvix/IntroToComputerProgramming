3cd67ff69a25b14fca192b2a358935b4d5b275b2
Max Vogel <mehvix@protonmail.com>
2019-02-06 09:46:12 -0600

    added comments

--- UnitOne-InputOutput/window_output.java
+++ UnitOne-InputOutput/window_output.java
@@ -3,14 +3,14 @@ Programmer: Max Vogel
 Demonstrates how 'output' works in java, now with JOptionPane
 */
 import javax.swing.*;
-import java.util.Random;
+import java.util.Random;  //Needed for Line 12
 
 public class window_output
 {     
    public static void main(String[] args)
    {  
-      int number =	new Random().nextInt((999999999 - 100000000) + 1) + 100000000;
-      ImageIcon picture = new ImageIcon("checkEm.jpg");
-      JOptionPane.showMessageDialog(null, number, "Check 'em:", JOptionPane.ERROR_MESSAGE, picture);
+      int number =	new Random().nextInt((999999999 - 100000000) + 1) + 100000000;  //Gets random number between 100000000 and 999999999
+      ImageIcon picture = new ImageIcon("checkEm.jpg");  //Gets image file
+      JOptionPane.showMessageDialog(null, number, "Check 'em", JOptionPane.ERROR_MESSAGE, picture);  //Actual window pop=up
    }
 }
