e27f01719670b657f2c6d8bf8d9a007f584c39b3
Max Vogel <mehvix@protonmail.com>
2019-02-06 08:51:31 -0600

    update

---
+++ UnitOne-InputOutput/terminal_output.java
@@ -0,0 +1,13 @@
+/*
+Programmer: Max Vogel
+Demonstrates how 'output' works in java
+*/
+
+public class terminal_output
+{
+   public static void main(String[] args)
+   {
+      System.out.println("Hello World!");
+      System.out.print("Here's a 2nd line");
+   }
+}
---
+++ UnitOne-InputOutput/window_output.java
@@ -0,0 +1,16 @@
+/*
+Programmer: Max Vogel
+Demonstrates how 'output' works in java, now with JOptionPane
+*/
+import javax.swing.*;
+import java.util.Random;
+
+public class window_output
+{     
+   public static void main(String[] args)
+   {  
+      int number =	new Random().nextInt((999999999 - 100000000) + 1) + 100000000;
+      ImageIcon picture = new ImageIcon("checkEm.jpg");
+      JOptionPane.showMessageDialog(null, number, "Check 'em:", JOptionPane.ERROR_MESSAGE);
+   }
+}
