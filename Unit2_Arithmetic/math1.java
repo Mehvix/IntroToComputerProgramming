e91290ab9d86a631aa068d0f3c2ece7d05fed542
Max Vogel <mehvix@protonmail.com>
2019-03-01 08:55:57 -0600

    Initial Commit

---
+++ Unit2_Arithmetic/math1.java
@@ -0,0 +1,18 @@
+/*
+Programmer: Max Vogel
+Goal:       This program asks 
+Github:     https://github.com/Mehvix/IntroToComputerProgramming/tree/master/Unit2_Arithmetic
+*/
+
+import javax.swing.*;
+public class math1
+{
+      public static void main(String[] args)
+      {
+         String num1, num2;
+         
+         //Gets numbers to add from user
+         num1 = JOptionPane.showInputDialog("Chose the first number that you want to add:");
+         System.out.println("User chose " + num1);
+      }  //end of main
+}//end of class
