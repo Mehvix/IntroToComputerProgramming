78b5bda54a63cc2c50f7cd603b887abc495e93cb
Max Vogel <vm27845@students.mcpasd.k12.wi.us>
2019-03-07 08:34:51 -0600

    Added responses

--- Unit2_Arithmetic/math_operators_keyin.java
+++ Unit2_Arithmetic/math_operators_keyin.java
@@ -1,7 +1,9 @@
 /*
-Programmer: Lori Hunt
-This program shows how the math operators
-work in java
+Programmer: Max Vogel
+Buzz URL:   https://middleton.agilixbuzz.com/student/109599350/activity/08069cda937741839783b67200229b19
+Github:     https://github.com/Mehvix/IntroToComputerProgramming/tree/master/Unit2_Arithmetic
+Goal:       This program shows how the math operators
+            work in java
 */
 public class math_operators_keyin
 {
@@ -23,13 +25,17 @@ public class math_operators_keyin
 }
 /*
 What is the significant difference between the first and second line?
+This first line adds seven and five as strings while the second line adds seven and five as integers
 
 What is the difference between the two division lines of code?
+The first division line is an integer, meaning it cannot have decimals. The second line has a double, meaning that it can contain decimals.
 
 What does the % symbol do?
+% means 'Mod', and it finds the remainder of two variables after you divide them
 
 Why do the last 2 lines of code give different answers?
+Because variables can do order of operations
 
 How does the order of operations work in Java?
-
+If you want to do the order of operations, you need it fo be in a variable
 */
