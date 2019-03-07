a0675dd8a38dcdf67c9da109973c8d656189b9b6
Max Vogel <vm27845@students.mcpasd.k12.wi.us>
2019-03-07 09:48:42 -0600

    Initial Commit

---
+++ default_example.java
@@ -0,0 +1,17 @@
+/*
+Programmer: Max Vogel
+Buzz URL:   
+Github:     https://github.com/Mehvix/IntroToComputerProgramming/tree/master/XXXXXXXXXXX   <---- Replace with unit
+Goal:       
+*/
+
+
+//import javax.swing.*; // Allows JOptionpane
+
+public class default_example
+{
+      public static void main(String[] args)
+      {         
+         // Set up for all variables
+      }//end of main
+}//end of class
